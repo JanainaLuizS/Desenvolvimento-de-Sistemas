d6d962a9422becf9c0b1f995a3db296dc5c0a64e
JanainaLuizS <janainasilvaluiz@gmail.com>
2026-08-30 14:44:44 -0300

    Update AnaliseTemperatura.java

--- Aula 2/ProjetoAnaliseTemperatura/src/exercicio14/AnaliseTemperatura.java
+++ Aula 2/ProjetoAnaliseTemperatura/src/exercicio14/AnaliseTemperatura.java
@@ -4,49 +4,48 @@ import java.util.Scanner;
 
 public class AnaliseTemperatura {
 
-	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);
-		
-		double[] temperatura = new double [7];
-		int i;
-		double maior;
-		double menor;
-		
-		for( i = 0; i < temperatura.length ;i++) {
-			System.out.print ((i+1) +"° valor: " );
-			temperatura[i] = scanner.nextInt();}
-		
-		
-		for( i = 0; i < temperatura.length ;i++) {
-			System.out.print ((i+1) +"° valor: " + temperatura );
-		
-		
-		 maior = temperatura[i];
-		 menor = temperatura [i];
-			
-			
-			if (temperatura[i]> maior) {
-				
-				maior = temperatura[i];
-			}
-			
-			if (temperatura[i]< menor) {
-				
-				menor= temperatura[i];
-			}
-		}
-		
-		
-		for( i = 0; i < temperatura.length ;i++) {
-			
-		System.out.println ((i+1) +"° temperatura: " + temperatura [i]);
-		}
-		
-		
-		System.out.println ("Maior temperatura: "  maior );
-		System.out.println ("Menor temperatura: "+ menor );
-	} 
-
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        double[] temperatura = new double[7];
+        int i;
+        
+        // 1. Leitura dos dados
+        for (i = 0; i < temperatura.length; i++) {
+            System.out.print((i + 1) + "° valor: ");
+            temperatura[i] = scanner.nextDouble(); // Corrigido para nextDouble()
+        }
+        
+        // 2. Inicialização do maior e menor FORA do laço
+        // Pegamos a primeira temperatura informada (índice 0)
+        double maior = temperatura[0];
+        double menor = temperatura[0];
+        
+        // 3. Laço para encontrar o maior e o menor
+        // Podemos começar em i = 1 pois o i = 0 já foi salvo acima
+        for (i = 1; i < temperatura.length; i++) {
+            
+            if (temperatura[i] > maior) {
+                maior = temperatura[i];
+            }
+            
+            if (temperatura[i] < menor) {
+                menor = temperatura[i];
+            }
+        }
+        
+        // 4. Exibição de todas as temperaturas
+        System.out.println("\n--- Temperaturas Cadastradas ---");
+        for (i = 0; i < temperatura.length; i++) {
+            System.out.println((i + 1) + "° temperatura: " + temperatura[i]);
+        }
+        
+        // 5. Exibição do maior e menor valor
+        System.out.println("\nMaior temperatura: " + maior);
+        System.out.println("Menor temperatura: " + menor);
+        
+        scanner.close();
+    } 
 }
 
 /*Objetivo
