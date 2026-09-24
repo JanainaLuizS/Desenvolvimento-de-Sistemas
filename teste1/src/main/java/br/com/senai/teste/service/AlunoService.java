bff78f311211fa81ba3d998e8d2579f22e2d7ee2
JanainaLuizS <janainasilvaluiz@gmail.com>
2026-09-23 21:44:08 -0300

    Add files via upload

---
+++ teste1/src/main/java/br/com/senai/teste/service/AlunoService.java
@@ -0,0 +1,44 @@
+package br.com.senai.teste.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.stereotype.Service;
+
+import br.com.senai.teste.model.Aluno;
+import br.com.senai.teste.repository.AlunoRepository;
+
+
+@Service
+public class AlunoService {
+    private final AlunoRepository alunoRepository;
+
+    public AlunoService(AlunoRepository alunoRepository) {
+        this.alunoRepository = alunoRepository;
+    }
+
+    public Aluno cadastrar(Aluno aluno) {
+        return alunoRepository.save(aluno);
+    }
+
+   public List<Aluno> listar() {
+        return alunoRepository.findAll();
+    }   
+
+    public Optional<Aluno> buscarPorId(int id) {
+        return alunoRepository.findById(id);
+    }
+
+    public Optional <Aluno> atualizar(int id, Aluno novosDados) {
+        Optional<Aluno> alunoEncontrado = alunoRepository.findById(id);
+        if (alunoEncontrado.isEmpty()) {
+            return Optional.empty();
+        } 
+        Aluno aluno = alunoEncontrado.get();
+
+        aluno.setNome(novosDados.getNome());
+        aluno.setEmail(novosDados.getEmail());
+          
+                return Optional.of(alunoRepository.save(aluno));
+        }
+    }
